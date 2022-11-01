afc8737a1785110ea24e1fd4d1b92b6c7e5f1880
Narlson Costa <naldrums@gmail.com>
2022-11-01 01:09:11 -0300

    Commit Final
    Commit Final

--- trilha-net-poo-desafio-main/Models/Smartphone.cs
+++ trilha-net-poo-desafio-main/Models/Smartphone.cs
@@ -4,7 +4,35 @@ using System.Text;
 
 namespace trilha_net_poo_desafio_main.Models
 {
-    class Smartphone
+    public abstract class Smartphone
     {
+        public string Numero { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+
+        protected Smartphone()
+        {
+        }
+
+        protected Smartphone(string numero, string modelo, string iMEI, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = iMEI;
+            Memoria = memoria;
+        }
+
+        public abstract void Ligar();
+         
+        
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+          
+    
     }
 }
--- trilha-net-poo-desafio-main/Program.cs
+++ trilha-net-poo-desafio-main/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using trilha_net_poo_desafio_main.Models;
 
 namespace trilha_net_poo_desafio_main
 {
@@ -6,7 +7,17 @@ namespace trilha_net_poo_desafio_main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Smartphone Nokia:");
+            Smartphone nokia = new Nokia(numero: "7591778744", modelo: "Modelo 1", iMEI: "555222", memoria: 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Wild Rifit");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Smartphone Iphone:");
+            Smartphone iphone = new Iphone(numero: "7591778397", modelo: "Modelo 2", iMEI: "5322214", memoria: 128);
+            iphone.Ligar();
+            iphone.InstalarAplicativo("Counter Strike");
         }
     }
 }
