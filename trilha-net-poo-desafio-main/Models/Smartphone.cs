6fa7762c11ed2248a01064562aeb599eb1b17ecb
Narlson Costa <naldrums@gmail.com>
2022-11-01 00:23:05 -0300

    Initial Commit
    Initial Commit

---
+++ trilha-net-poo-desafio-main/Models/Smartphone.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trilha_net_poo_desafio_main.Models
+{
+    class Smartphone
+    {
+    }
+}
---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trilha_net_poo_desafio_main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
